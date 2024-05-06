46bd2d9e82eddfe7e24a0379a7b1d0bc138b38e2
Alexis Marroquin <a.marroquin@gmail.com>
2024-05-05 23:43:09 -0400

    completed sign-in/sign-up pages

---
+++ app/_layout.jsx
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { SplashScreen,Slot, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout  = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+  });
+
+  useEffect(() => {
+    if(error) throw error;
+
+    if(fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error])
+
+  if(!fontsLoaded && !error) return null;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/*<Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
+    </Stack>
+    
+  )
+}
+
+export default RootLayout 
